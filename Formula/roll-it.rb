38b8bdf63287975dc64f75af07436eb7cf1137dc
Patrick Kilgore <git@pck.email>
2021-10-09 23:53:31 -0500

    roll-it 2.1.0 (new formula)
    Closes #2.
    
    Signed-off-by: Patrick Kilgore <git@pck.email>

---
+++ Formula/roll-it.rb
@@ -0,0 +1,17 @@
+class RollIt < Formula
+  desc "Automatically roll your AWS IAM access key and secret key"
+  homepage "https://github.com/pckilgore/roll-it/wiki"
+  url "https://github.com/pckilgore/roll-it/archive/refs/tags/v2.1.0.tar.gz"
+  sha256 "cdad790d0a450ca22d26ac4471f45cb7d68d072d05b164f70df6c53106b2dcbd"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-ldflags=-s -w", "-o", "#{bin}/rollit"
+  end
+
+  test do
+    shell_output("#{bin}/rollit", 1)
+  end
+end
